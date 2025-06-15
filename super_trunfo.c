dda98b8f633bbec737fd0bd2a52d4a1db3be8283
klehvymn <klehvymn@gmail.com>
2025-06-15 11:04:18 -0500

    Create super_trunfo.c

---
+++ super_trunfo.c
@@ -0,0 +1,126 @@
+#include <stdio.h>
+
+int main() {
+
+    char carta1, carta2;
+    char estado1, estado2;
+    char nomeCidade1, nomeCidade2;
+    char codigoCarta1[3], codigoCarta2[3];
+    int populacao1, populacao2;
+    float pib1, pib2;
+    int PontosTur1, PontosTur2;
+    float area1, area2;
+    
+
+//    char carta1=Carta_1, carta2=Carta_2;
+//    char estado1=a, estado2=a;
+//    char nomeCidade1=a, nomeCidade1=a;
+//    char codigoCarta1[3]=a, codigoCarta2[3]=a;
+//    int populacao1=0, populacao2=0;
+//    int pib1=0, pib2=0;
+//    int PontosTur1=0, PontosTur2=0;
+//    float area1=0, area2=0;    
+
+   
+    // --- Coleta de dados para a Carta 1 ---
+    printf("--- Insira os dados da Carta 1 ---\n");
+
+    
+    printf("Insira o codigo da Carta1: \n");
+    scanf("%3s", codigoCarta1);
+
+
+ //   printf("Insira o estado (A a H): ");
+ //   scanf("%s", estado1); 
+
+    
+ //   printf("Insira o nome da Cidade: ");
+ //   scanf("%s" nomeCidade1);
+
+        
+ //   printf("Qual a populacao: ");
+ //   scanf("%d", populacao1);
+ 
+        
+ //   printf("Quak a area (em km²): ");
+ //   scanf("%.2d", area1);
+
+        
+ //   printf("Quak o PIB (em bilhoes de reais): ");
+ //   scanf("%.2d", pib1);
+
+        
+ //   printf("Numero de Pontos Turisticos: ");
+ //   scanf("%d", PontosTur1);
+
+        
+ //   printf("\n"); 
+
+    // --- Coleta de dados para a Carta 2 ---
+ //   printf("--- Insira os dados da Carta 2 ---\n");
+
+    printf("Insira o codigo da Carta2: \n");
+    scanf("%3s", codigoCarta2);
+
+
+ //   printf("Insira o estado (A a H): ");
+ //   scanf("%s", estado2); 
+
+    
+ //   printf("Insira o nome da Cidade: ");
+ //   scanf("%s" nomeCidade2);
+
+        
+ //   printf("Qual a populacao: ");
+ //   scanf("%d", populacao2);
+ 
+        
+ //   printf("Quak a area (em km²): ");
+ //   scanf("%.2d", area2);
+
+        
+ //   printf("Quak o PIB (em bilhoes de reais): ");
+ //   scanf("%.2d", pib2);
+
+        
+ //   printf("Numero de Pontos Turisticos: ");
+ //   scanf("%d", PontosTur2);
+
+        
+//    printf("\n");
+
+    // --- Exibição dos dados da Carta 1 ---
+    printf("--- Informacoes da Carta 1 ---\n");
+  //  printf("Estado: %c\n", carta1, estado);
+    printf("Codigo: %s\n", codigoCarta1);
+  //  printf("Nome da Cidade: %s\n", carta1, nomeCidade);
+  //  printf("Populacao: %d\n", carta1, populacao);
+  //  printf("Area: %.2f km²\n", carta1, areaKm2); 
+  //  printf("PIB: %.2f bilhoes de reais\n", carta1, pib);
+  //  printf("Numero de Pontos Turisticos: %d\n", carta1, PontosTur);
+
+  //  printf("\n"); 
+
+    // --- Exibição dos dados da Carta 2 ---
+    printf("--- Informacoes da Carta 2 ---\n");
+  //  printf("Estado: %c\n", carta2, estado);
+    printf("Codigo: %s\n", codigoCarta2);
+  //  printf("Nome da Cidade: %s\n", carta2, nomeCidade);
+  //  printf("Populacao: %d\n", carta2, populacao);
+  //  printf("Area: %.2f km²\n", carta2, areaKm2);
+  //  printf("PIB: %.2f bilhoes de reais\n", carta2, pib);
+  //  printf("Numero de Pontos Turisticos: %d\n", carta2, PontosTur);
+
+
+    // Exibindo assinatura
+    
+    printf("\n");
+    printf("\n");
+    printf("#=======================#\n");
+    printf("#  By Cleverson Lima!!! #\n");
+    printf("#=======================#\n");
+    printf("\n");
+    printf("\n");
+
+return 0;
+}
